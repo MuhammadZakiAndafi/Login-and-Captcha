7faf130d253a2051e65f3025fbb9b5e2dff7967a
MuhammadZakiAndafi <144996436+MuhammadZakiAndafi@users.noreply.github.com>
2023-12-13 23:14:00 +0700

    Add files via upload

---
+++ Login and Captcha/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+
+    //polymorphisme
+    DataCustomer Zaki = new Customer();
+
+    Login Andafi = new Login();
+    Andafi.login();
+
+    //inputkan data
+    Zaki.inputDataPelanggan();
+    
+    //mencetak struk
+    Zaki.Struk();
+
+    }
+}
